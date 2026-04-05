fcf60dedc9ac84e68dc3a1f577433346550d28c8
MedAiResearch <nikitawallet783@gmail.com>
2026-04-05 14:51:27 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import os
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
 workers = 1
-worker_class = "gevent"
-worker_connections = 10
+worker_class = "gthread"
+threads = 4
 timeout = 300
 graceful_timeout = 60
 keepalive = 5
